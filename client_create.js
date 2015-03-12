5b7421f62c897af0f79a2bb7cff48d49f5a6cd15
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-03-12 18:55:44 +0530

    read json files and checks for student record is already exists or not.

--- client_create.js
+++ client_create.js
@@ -4,39 +4,46 @@ try {
     if ((http === undefined)) throw new Error( " Can't access http module" );
     if ((jsonReader === undefined)) throw new Error( " Can't access json-reader module" );
 
-
-    var options = {
-        host: '127.0.0.1',
-        port: '1337',
-        path: '/api/student',
-        method: 'PUT',
-        headers: {'accept': 'text/plain'}
-//        headers: {'accept': 'application/json'}
-    };
-
-    var callback = function(response) {
-        console.log("Status code: ", response.statusCode);
-        console.log("Status: ", response.statusMessage);
-        var chunks;
-        response.on('data', function (chunk) {
- //           chunks += chunk;
-            console.log('BODY: '+ chunk);
-        });
- //       response.on('end', function () {
- //           console.log('BODY: ', chunks);
- //       });
-    }
-
-
     jsonReader.jsonObject("./sourceFiles/student.json", function ( err, object ) {
         if(err) {  //Throw an error if failed to read JSON file.
             console.log(err);
-        } else {
+        } else {  //Sends json object to the server.
+
+            var options = {
+                host: '127.0.0.1',
+                port: '1337',
+                path: '/api/student',
+                method: 'PUT',
+                headers: {
+                    'content-type': 'text/plain'
+                }
+            };//  'application/json' 'text/plain' 
+
+            var callback = function(response) {
+        //        var chunks;
+                response.on('data', function (chunk) {
+         //           chunks += chunk;
+                    console.log('BODY: '+ chunk);
+                });
+         //       response.on('end', function () {
+         //           console.log('BODY: ', chunks);
+         //       });
+            }
+
+
             var request = http.request(options, callback);
-            request.write( JSON.stringify(object) );
-            request.end();
+            request.write( JSON.stringify(object), function (bSuccess) {
+                if(bSuccess) {
+                    console.log("file not send.");
+                    request.end();
+                } else {
+                    console.log("file send.");
+                    request.end();
+                }
+            });
         }
     });
+    
 } catch (err) {
     console.log(err);
 }
--- httpServer.js
+++ httpServer.js
@@ -2,35 +2,147 @@ try {
     var http = require("http");
     var fs = require('fs');
     var qs = require('querystring');
-    var url = require('url');
+    var async = require("async");
+    var jsonReader = require("json-reader");
     
     //Checks the required modules are available or not.
     if (http === undefined) throw new Error( " Can't access http module" );
     if (fs === undefined) throw new Error( " Can't access fs module" );
     if (qs === undefined) throw new Error( " Can't access qs module" );
-    if (url === undefined) throw new Error( " Can't access url module" );
+    if (async === undefined) throw new Error( " Can't access async module" );
+    if (jsonReader === undefined) throw new Error( " Can't access json-reader module" );
+
+
+    //  Read all source files parallely.
+    var readSourceFiles = function (cb) {
+        async.parallel([
+        	function (callback) {  //Reading students.json
+                jsonReader.jsonObject("./sourceFiles/students.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read students.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_1.json
+                jsonReader.jsonObject("./sourceFiles/sub_1.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_1.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_2.json
+                jsonReader.jsonObject("./sourceFiles/sub_2.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_2.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_3.json
+                jsonReader.jsonObject("./sourceFiles/sub_3.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_3.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_4.json
+                jsonReader.jsonObject("./sourceFiles/sub_4.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_4.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_5.json
+                jsonReader.jsonObject("./sourceFiles/sub_5.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_5.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            }
+        ],
+        // optional callback 
+        function(err, results){
+            if (err) {  //Returns an error if error occured in reading any of the subject files in json objects.
+                return cb(new Error(" Can't read source json file."), null);
+            } else {  //Returns array of json objects.
+                return cb(null, results);
+            }
+        });//  async.parallel().
+    }//  readSourceFiles(parallel).
+
 
     var server = http.createServer ( function (req, res) {
         var folderName = req.url.split('/')[1];  //devided url on base of "/".
         folderName = folderName.toLowerCase();  //Converte folder name to lower case.
+
         if ( folderName === "favicon.ico" ) {
-            res.end("test");   //Avoid un necessory execution of code.
-        } else {  //Checkes the requseted types and call respective functions.
-            if ( req.method === 'GET' ) {
-                res.end("GET request.");
-            } else if ( req.method === 'PUT' ) {
-            	;
-            	;
-            	//var queryObject = url.parse(req.url,true).query;
-//            	var chunks;
-            	req.on('data', function (chunk) {
-//            		chunks += chunk;
-            		console.log('BODY: '+ chunk);
-            	});
-                res.end("PUT request.");
-            }
-        }
-    });
+            res.end();   //Avoid un necessory execution of code.
+        } else {  //Checkes the requset type and call respective functions for performing CRUD operations.
+
+            if ( req.method === 'PUT' ) {//  If received PUT request from client then create the record.
+                var chunks = "";
+                req.on('data', function (chunk) {
+                    chunks += chunk;
+                });
+                req.on('end', function () {
+                    var newStudent = JSON.parse(chunks);
+                    readSourceFiles( function (err, jsonObjects) {
+                        if (err) {  //send error message if fails to read source files.
+                            console.log(err);
+                            res.end("Error in reading resource json files.");
+                        } else {
+                            if (jsonObjects[0].students === undefined ) {  //sends error message if Student element is not found in student.json.
+                                console.log("Error: Student element is not found in student.json.");
+                                res.end("Student element is not found in student.json.");
+                            } else {
+
+                                var students = jsonObjects[0].students;
+                                var isRecordPresent = true;
+                                for (x in students) {  //Saparately access every students.
+                                    if (students[x].email === newStudent.email) {
+                                        console.log("Error: Student with the same email is already present.");
+                                        res.end("Student with the same email is already present.");
+                                        break;
+                                    } else if (students[x].name === newStudent.name) {
+                                        console.log("Error: Student with the same name is already present.");
+                                        res.end("Student with the same name is already present.");
+                                        break;
+                                    } else {  //student is alrready not in record. So add a new record.
+                                        isRecordPresent = false;
+                                    }
+                                }
+                                if ( !isRecordPresent ) {
+                                    console.log("Student is not already in record");
+                                    ;
+                                    ;
+                                    res.end("PUT request.");
+                                } 
+                            }
+                        }
+                    });//  readSourceFiles().
+                });//  req.on('end',).
+            }//  if ( req.method === 'PUT' )
+            //res.end("temp res.end().");
+
+        }//  ( folderName === "favicon.ico" )
+    });//  http.createServer().
 
     server.listen( 1337, "127.0.0.1", function() {
         console.log( "Listening on: 127.0.0.1: 1337" );
